bdf4d35b773d297e269c8c0e51b6037e99da9c18
wApptastic <info@wapptastic.com>
2014-09-13 02:16:26 +0200

    Fixes

--- routes/employees_private.rb
+++ routes/employees_private.rb
@@ -26,33 +26,6 @@
     haml :"employee/show"
   end
    
-  get "/employees/edit/:id/?" do
-    login_required  
-    @employee = Employee.get(params[:id])
-    logger.info "Employee updated form loaded for employee with id " + params[:id].to_s 
-    @title = "Edit Employee Info"
-    haml :"employee/edit"
-  end
-   
-  put "/employees/:id/?" do
-    login_required  
-    employee = Employee.get(params[:id])
-    logger.info "Employee updated with id " + params[:id].to_s
-    employee.update(:firstname => params[:firstname], :lastname => params[:lastname], :phone => params[:phone])
-    redirect "/employees"
-  end
-   
-  get '/employees/delete/:id/?' do
-    login_required  
-    @employee = Employee.get(params[:id])
-    logger.info "Employee deleted with id " + params[:id].to_s
-    haml :"employee/delete"
-  end
-
-  delete '/employees/delete/:id/?' do
-    login_required  
-    Employee.get(params[:id]).destroy
-    redirect '/employees'  
-  end
+  
 
  
