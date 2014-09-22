0afd99067e92396935ce569835bf8f83dd8f1476
wApptastic <info@wapptastic.com>
2014-09-22 15:16:46 +0200

    Fix

--- models/employee.rb
+++ models/employee.rb
@@ -1,10 +1,10 @@
 class Employee
   include DataMapper::Resource  
   property :id,           Serial,	key: true, unique_index: true
-  property :company_id,   Integer,  required:true
+  property :company_id,   Integer,  required: true
   property :firstname,    String,	required: true, length: 1..20
   property :lastname,     String,   required: true, length: 1..30
-  property :phone,        String,   required: true, length: 1..13
+  property :phone,        String,   required: true, length: 1..20
   property :email,		  String,	required: true, length: 1..60
   property :created_at,   DateTime
   property :updated_at,   DateTime
