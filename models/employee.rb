02c694c33bd0e865eba80cff09d1ef5fc20418aa
wApptastic <info@wapptastic.com>
2014-09-22 14:08:51 +0200

    Fix

--- models/employee.rb
+++ models/employee.rb
@@ -5,7 +5,7 @@ class Employee
   property :firstname,    String,	required: true, length: 1..20
   property :lastname,     String,   required: true, length: 1..30
   property :phone,        String,   required: true, length: 1..13
-  property :email,		  String,	:required => true, length: 1..60, :format => :email_address
+  property :email,		  String,	required: true, length: 1..60, :format => :email_address
   property :created_at,   DateTime
   property :updated_at,   DateTime
 
