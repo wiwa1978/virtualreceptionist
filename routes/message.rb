e96ebd0814ba81b9c59df768bb55ffb556bed966
Wim <wauters1978@gmail.com>
2020-10-04 11:31:26 +0200

    Update message.rb

--- routes/message.rb
+++ routes/message.rb
@@ -1,6 +1,6 @@
 before do
-  account_sid = "AC2b99feef2c2fcaad4bea74b969cfb35c"
-  auth_token = "dab2b9ce1d0b0f61e48982afce151552"
+  account_sid = "***"
+  auth_token = "***"
   @twilio_number = "+14846624263"
 
   #TWILIO_ACCOUNT_SID  =   ENV['TWILIO_ACCOUNT_SID'] || TWILIO_ACCOUNT_SID
